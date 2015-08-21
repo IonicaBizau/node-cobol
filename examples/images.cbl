aa03113466b09927c4f926013be0e3535735f991
Ionică Bizău <bizauionica@yahoo.com>
2015-08-21 15:43:28 +0300

    Added images.cbl

---
+++ examples/images.cbl
@@ -0,0 +1,49 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IMAGES.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "1ii11111111111f01tLC111111iii1tfCGGt;;;;;;;;;;;;;;".
+           DISPLAY "t11111iiii1111CLt8@0111111111fCG0088C;;;;;;;;;;;;;".
+           DISPLAY "Lt1iii;;ii1111G1f@8G1111i111L88@0GC0@L;;;;;;;;;;;;".
+           DISPLAY "1iiii;;;ii111tC1L88G111ii1i1G08@8GtLG8Lf1;;;;;;;;;".
+           DISPLAY ";;;i;;;iii111tfiC@8L111;iii1fLLCGGLGGCCG01;;;;;;;;".
+           DISPLAY "t;;i;;;iii111t11080L11ii111iLGCG0088fLC08Lii;;;;;;".
+           DISPLAY "tt;;;;iiiii1111f880C1111111ifCC000Gf1CGCCffti;;;;;".
+           DISPLAY "itt;;;iiiii1111f@0Ct11111i;i;fCCCGGGLti;i1111;111i".
+           DISPLAY "1ift;iiiiiii111tf1;i11ii1tfLCLLCCG08@0Li;;i1fi111i".
+           DISPLAY "i11f1iiiiiiii111iiii11i;1GGGG0CLCG8@@@@8Lii1f11ti;".
+           DISPLAY "11i1f1ii;i1ii11111ii1i;iLGCGCG8CG0088888@G1fLtfLf1".
+           DISPLAY "it111f1iii1ii111iiiiiiiiCGGGGC00G00G88888@G1ttfLLf".
+           DISPLAY "1i11iif1;iiiiiii1iiiiii1CCGGGGG008000000008t1LtfLL".
+           DISPLAY "11it1ittiiii;iiiiii11iitGCG0G00088000GG0008LfCffLC".
+           DISPLAY "1111tii1ti1i;iiiii;11iitGCG0088088000GCG000LttffLL".
+           DISPLAY "11;i11ii1ftt1i1iii;;iiifGCG0000880000GCG000L1tfLfL".
+           DISPLAY "i;ii;i1i1ttfL1iii1i;iitCGLCCG000800000CGGGGt1ttttt".
+           DISPLAY "i;iiii11i1tff1iiiii;iL0CCCCfCGGGCG0G0GLCGGC1ttt11C".
+           DISPLAY "ii1iii;11;;tffLft1;1C88GCCCGGGGGGG0GCGLLG0LittttC0".
+           DISPLAY "1tt1iii11t1fLLLLGffG0GLLCC8808800G0000CG00ti1ttG8t".
+           DISPLAY "L1itt111t8GLLLfCCC00Ct1if08808888GG0GG00Gfiii1GGt1".
+           DISPLAY "1ttttfitLG@@0G0CC008@88GLC88088888GGG0GLt1iiiC0t1t".
+           DISPLAY "tfLti1fLLLG@0CGG08@@@@@@@0G888888GGGGLffGtiit8L11f".
+           DISPLAY "fLLLti1LCftLCG0888888@@@@@80000GGCLLLLG88ti1GG1t1G".
+           DISPLAY "LLLLfLLLffffffLCC088888880GGGGCCCGG888888LffCf11f0".
+           DISPLAY "LCCLLCLt1111tft1tfLCLLLLLCCCCCCG088@888GfLCLtttt0G".
+           DISPLAY "CCLLCf1111iifLttCCLLGGG088888888888880LfLCCLtt1C8G".
+           DISPLAY "CLLCfi111iiiLftfG08888@@@@@@@8888888CffLCCLtttt80G".
+           DISPLAY "             Rear Admiral Grace Hopper            ".
+           DISPLAY "                      * * *                       ".
+           DISPLAY "Fifty years ago, each computer maker used  its own".
+           DISPLAY "programming  languages to tell a computer what  to".
+           DISPLAY "do. In 1959, a group of programmers devised COBOL,".
+           DISPLAY "a  COmmon,  Business-Oriented  Language.  Programs".
+           DISPLAY "written   in   COBOL   could  run on more than one".
+           DISPLAY "manufacturer’s computer.".
+           DISPLAY "In a 1960   test,  the same COBOL   programs   ran".
+           DISPLAY "successfully on   two computers built by different".
+           DISPLAY "manufacturers.".
+           DISPLAY "(americanhistory.si.edu: COBOL)".
+       PROGRAM-DONE.
+           STOP RUN.
